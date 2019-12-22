bff652fb2dc17d54c58a013d2bd3239c14dd045b
Uice Lu <uicestone@gmail.com>
2019-12-22 17:00:12 +0800

    auto update due careItems pre save

--- src/models/Machine.ts
+++ src/models/Machine.ts
@@ -1,7 +1,10 @@
-import { prop, getModelForClass, plugin } from "@typegoose/typegoose";
+import { prop, getModelForClass, plugin, pre } from "@typegoose/typegoose";
 import updateTimes from "./plugins/updateTimes";
 
 @plugin(updateTimes)
+@pre<Machine>("save", function() {
+  this.updateDueCareItems();
+})
 class Machine {
   @prop()
   num: string;
@@ -20,6 +23,25 @@ class Machine {
 
   @prop()
   model: string;
+
+  @prop()
+  careItems: {
+    name: string;
+    cycle: number;
+    cycleUnit: string;
+    last: number;
+    cycleLeft: number;
+  }[];
+
+  updateDueCareItems() {
+    this.careItems = this.careItems.map(item => {
+      console.log(item.last);
+      return {
+        ...item,
+        cycleLeft: item.cycle - (this.totalHours - (item.last || 0))
+      };
+    });
+  }
 }
 
 export default getModelForClass(Machine, {
